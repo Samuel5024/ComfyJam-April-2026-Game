f496fbd7f4308fb54ebacf00e1a5a25875f00217
Wilkerson <WILKERC2@my.erau.edu>
2026-04-11 19:13:23 -0700

    add time scoring

---
+++ Beeventure-ComfyJam2026/Assets/Scripts/ConstantData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ConstantData
+{
+    public static int nectar = 0;
+    //add upgrade data here
+}
--- Beeventure-ComfyJam2026/Assets/Scripts/HiveManager.cs
+++ Beeventure-ComfyJam2026/Assets/Scripts/HiveManager.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class HiveManager : MonoBehaviour
 {
+    [SerializeField] float maxNectar;
+    [SerializeField] float minNectar;
+    [SerializeField] float timeToMinNectar;
+    private float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+    }
     [SerializeField] private string missionCompleteScene;
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -10,6 +18,10 @@ public class HiveManager : MonoBehaviour
         {
             if (collision.gameObject.GetComponent<FlowerCollector>().HasAllFlowers())
             {
+                Debug.Log("time (Seconds): " + (Time.time - startTime));
+                Debug.Log("time Proportion: " + Mathf.Clamp((Time.time - startTime) / timeToMinNectar, 0f, 1f));
+                ConstantData.nectar += Mathf.CeilToInt(Mathf.Lerp(maxNectar, minNectar, Mathf.Clamp((Time.time - startTime) / timeToMinNectar, 0f, 1f)));
+                Debug.Log("Nectar: " + ConstantData.nectar);
                 SceneManager.LoadScene(missionCompleteScene);
             }   
         }
