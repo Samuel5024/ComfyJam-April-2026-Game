4c9f65b6b4cf8efd4097c6976a41f06ec6aca87c
Wilkerson <WILKERC2@my.erau.edu>
2026-04-12 17:46:31 -0700

    add dynamic arena scaling

--- Beeventure-ComfyJam2026/Assets/Scripts/MissionMaker.cs
+++ Beeventure-ComfyJam2026/Assets/Scripts/MissionMaker.cs
@@ -11,6 +11,7 @@ public class MissionMaker : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        transform.localScale = transform.localScale * SpawnRadius;
         missionFlowers = new GameObject[uniqueFlowersPerMission];
         SetFlowerPool();
         SpawnFlowers();
