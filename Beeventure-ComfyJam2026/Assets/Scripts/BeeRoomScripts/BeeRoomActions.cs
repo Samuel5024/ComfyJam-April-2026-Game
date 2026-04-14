917b405d85cfb4ea0b5c1a6c17c514c9add3d934
Wilkerson <WILKERC2@my.erau.edu>
2026-04-13 21:05:20 -0700

    Add hat purchase menu

--- Beeventure-ComfyJam2026/Assets/Scripts/BeeRoomScripts/BeeRoomActions.cs
+++ Beeventure-ComfyJam2026/Assets/Scripts/BeeRoomScripts/BeeRoomActions.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,7 @@ public class BeeRoomActions : MonoBehaviour
 {
     //collins insane ramblings: If i use delegates to pass an upgrade function to the upgrade button it might be more efficiant
     [SerializeField] string NextScene;
+    [SerializeField] int hatBaseCost = 1000;
     public void LetsFlyButton()
     {
         SceneManager.LoadScene(NextScene);
@@ -18,5 +20,40 @@ public class BeeRoomActions : MonoBehaviour
     public void SelectChair()
     {
         Debug.Log("Chair");
+    }
+
+    public void SelectSpeedHat(int tier)
+    {
+        if (ConstantData.nectar >= tier * hatBaseCost)
+        {
+            ConstantData.nectar -= tier * hatBaseCost;
+            ConstantData.hatTeir = tier;
+            ConstantData.hatType = HatType.Speed;
+            return;
+        }
+        Debug.Log("Brokey poor poor broke");
+    }
+
+    public void SelectMoneyHat(int tier)
+    {
+        if (ConstantData.nectar >= tier * hatBaseCost)
+        {
+            ConstantData.nectar -= tier * hatBaseCost;
+            ConstantData.hatTeir = tier;
+            ConstantData.hatType = HatType.Money;
+            return;
+        }
+        Debug.Log("Brokey poor poor broke");
+    }
+    public void SelectTrackingHat(int tier)
+    {
+        if (ConstantData.nectar >= tier * hatBaseCost)
+        {
+            ConstantData.nectar -= tier * hatBaseCost;
+            ConstantData.hatTeir = tier;
+            ConstantData.hatType = HatType.Detection;
+            return;
+        }
+        Debug.Log("Brokey poor poor broke");
     }
 }
