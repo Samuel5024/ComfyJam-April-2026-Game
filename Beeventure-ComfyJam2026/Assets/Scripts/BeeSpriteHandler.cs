05607264e45a54e5d3b665a2e7504665611aead7
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2026-04-14 19:22:10 -0700

    add bee hat swapping

--- Beeventure-ComfyJam2026/Assets/Scripts/BeeSpriteHandler.cs
+++ Beeventure-ComfyJam2026/Assets/Scripts/BeeSpriteHandler.cs
@@ -2,15 +2,85 @@ using UnityEngine;
 
 public class BeeSpriteHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Sprite SpeedHatT1;
+    [SerializeField] private Sprite SpeedHatT2;
+    [SerializeField] private Sprite SpeedHatT3;
+    
+    [SerializeField] private Sprite MoneyHatT1;
+    [SerializeField] private Sprite MoneyHatT2;
+    [SerializeField] private Sprite MoneyHatT3;
+
+    [SerializeField] private Sprite LocatorSpriteT1;
+    [SerializeField] private Sprite LocatorSpriteT2;
+    [SerializeField] private Sprite LocatorSpriteT3;
+
+    [SerializeField] private SpriteRenderer HatSprite;
+    private SpriteRenderer beeSprite;
+
+    private void Start()
     {
-        
+        beeSprite = gameObject.GetComponent<SpriteRenderer>();
+        SetBeeHat();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetBeeColor(Color beeColor)
     {
-        
+        beeSprite.color = beeColor;
+    }
+
+    private void SetBeeHat()
+    {
+        HatSprite.color = Color.white;
+        switch (ConstantData.hatType)
+        {
+            case HatType.Speed:
+
+                switch (ConstantData.hatTeir)
+                {
+                    case 1:
+                        HatSprite.sprite = SpeedHatT1;
+                        break;
+                    case 2:
+                        HatSprite.sprite = SpeedHatT2;
+                        break;
+                    case 3:
+                        HatSprite.sprite = SpeedHatT3;
+                        break;
+                }
+                break;
+            case HatType.Money:
+
+                switch (ConstantData.hatTeir)
+                {
+                    case 1:
+                        HatSprite.sprite = MoneyHatT1;
+                        break;             
+                    case 2:                
+                        HatSprite.sprite = MoneyHatT2;
+                        break;             
+                    case 3:                
+                        HatSprite.sprite = MoneyHatT3;
+                        break;
+                }
+                break;
+            case HatType.Detection:
+
+                switch (ConstantData.hatTeir)
+                {
+                    case 1:
+                        HatSprite.sprite = LocatorSpriteT1;
+                        break;
+                    case 2:
+                        HatSprite.sprite = LocatorSpriteT2;
+                        break;
+                    case 3:
+                        HatSprite.sprite = LocatorSpriteT3;
+                        break;
+                }
+                break;
+            default:
+                HatSprite.color = Color.clear;
+                break;
+        }
     }
 }
