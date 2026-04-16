0fa1d1ab4bd0feb196575b12f176b988ebc3f5bf
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2026-04-16 09:33:32 -0700

    finish indicator upgrades

--- Beeventure-ComfyJam2026/Assets/Scripts/FlowerLocator.cs
+++ Beeventure-ComfyJam2026/Assets/Scripts/FlowerLocator.cs
@@ -5,34 +5,35 @@ using static UnityEngine.GraphicsBuffer;
 public class FlowerLocator : MonoBehaviour
 {
     [SerializeField] private float indicatorDistanceFromPlayer = 3f;
-    [SerializeField] private GameObject indicator;
+    [SerializeField] private IndicatorMovement indicator;
     [SerializeField] private GameObject hive;
     [SerializeField] private float smoothing = 0.01f;
     [SerializeField] private float ShowDistance = 0.5f;
-
-    private Vector3 DesiredLocation;
-    private Quaternion DesiredRotation;
+    [SerializeField] private float refreshRate = 0.05f;
 
     public GameObject[] flowers;
     private GameObject player;
+    private IndicatorMovement[] indicators;
 
     private void Start()
     {
-        if (ConstantData.hatType != HatType.Detection)
+        indicator.SetSmoothing(smoothing);
+        player = FindAnyObjectByType<BeeMovement>().gameObject;
+
+        if (ConstantData.hatType == HatType.Detection && ConstantData.hatTeir > 1)
         {
-            indicator.SetActive(false);
+            indicators = new IndicatorMovement[flowers.Length];
+            
+            for(int i = 0; i < indicators.Length; i++)
+            {
+                indicators[i] = Instantiate(indicator);
+            }
+            StartCoroutine(T2IndicatorRoutine());
+            return;
         }
 
-        indicator.SetActive(true);
-        player = FindAnyObjectByType<BeeMovement>().gameObject;
+        indicator.gameObject.SetActive(true);
         StartCoroutine(IndicatorRoutine());
-        indicator.transform.position = DesiredLocation;
-    }
-
-    private void Update()
-    {
-       indicator.transform.position = Vector3.Lerp(indicator.transform.position, DesiredLocation, smoothing);
-        indicator.transform.rotation = Quaternion.Lerp(indicator.transform.rotation, DesiredRotation, smoothing);
     }
 
     private IEnumerator IndicatorRoutine()
@@ -40,9 +41,9 @@ public class FlowerLocator : MonoBehaviour
         while (true)
         {
             Vector3 directionToFlower = (GetNearestFlower().transform.position - player.transform.position).normalized;
-            DesiredLocation = directionToFlower * indicatorDistanceFromPlayer + player.transform.position;
-            DesiredRotation = Quaternion.LookRotation(Vector3.forward, directionToFlower);
-            yield return new WaitForSeconds(0.1f);
+            indicator.SetDesiredLocation(directionToFlower * indicatorDistanceFromPlayer + player.transform.position);
+            indicator.SetDesiredRotation(Quaternion.LookRotation(Vector3.forward, directionToFlower));
+            yield return new WaitForSeconds(refreshRate);
 
             if(ConstantData.hatType == HatType.Detection)
             {
@@ -51,10 +52,35 @@ public class FlowerLocator : MonoBehaviour
 
             if(Vector3.Distance(GetNearestFlower().transform.position, player.transform.position) < ShowDistance)
             {
-                indicator.SetActive(true);
+                indicator.gameObject.SetActive(true);
                 continue;
             }
-            indicator.SetActive(false);
+            indicator.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator T2IndicatorRoutine()
+    {
+        while (true)
+        {
+            for (int i = 0; i < flowers.Length; i++)
+            {
+                if (flowers[i] == null)
+                {
+                    indicators[i].gameObject.SetActive(false);
+                    continue;
+                }
+                Vector3 directionToFlower = (flowers[i].transform.position - player.transform.position).normalized;
+                indicators[i].SetDesiredLocation(directionToFlower * indicatorDistanceFromPlayer + player.transform.position);
+                indicators[i].SetDesiredRotation(Quaternion.LookRotation(Vector3.forward, directionToFlower));
+                if(ConstantData.hatTeir > 2)
+                {
+                    indicators[i].transform.localScale = Vector3.one * Mathf.Clamp(1 / (Vector3.Distance(flowers[i].transform.position, player.transform.position) / 10),0.3f, 1);
+                }
+            }
+            indicator.SetDesiredLocation((hive.transform.position - player.transform.position).normalized * indicatorDistanceFromPlayer + player.transform.position);
+            indicator.SetDesiredRotation(Quaternion.LookRotation(Vector3.forward, (hive.transform.position - player.transform.position).normalized));
+            yield return new WaitForSeconds(refreshRate);
         }
     }
 
